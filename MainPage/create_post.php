cca6c292a3bc2a658bc98161a60a2fb17e7a463a
johngiampapa01 <144369835+johngiampapa01@users.noreply.github.com>
2024-03-13 15:53:46 -0400

    Add files via upload

--- MainPage/create_post.php
+++ MainPage/create_post.php
@@ -42,6 +42,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         if ($stmt_insert->execute()) {
             // Post created successfully
+
+            //NEW insert the group into the database by taking title name of post and making it group name
+            $sql_insert_group = "INSERT INTO groups (group_name) VALUES (?)";
+            $stmt_insert_group = $conn->prepare($sql_insert_group);
+            $stmt_insert_group->bind_param("s", $title);
+            $stmt_insert_group->execute();
+
+            // NEW get the group_id of the group just created
+            $sql_get_group_id = "SELECT group_id FROM groups WHERE group_name = ?";
+            $stmt_get_group_id = $conn->prepare($sql_get_group_id);
+            $stmt_get_group_id->bind_param("s", $title);
+            $stmt_get_group_id->execute();
+            $result_group_id = $stmt_get_group_id->get_result();
+            $row_group_id = $result_group_id->fetch_assoc();
+            $group_id = $row_group_id['group_id'];
+
+            // NEW Insert the user_id and group_id into the group_members table
+            $sql_insert_group_members = "INSERT INTO group_members (user_id, group_id) VALUES (?, ?)";
+            $stmt_insert_group_members = $conn->prepare($sql_insert_group_members);
+            $stmt_insert_group_members->bind_param("ii", $userid, $group_id);
+            $stmt_insert_group_members->execute();
+
             header("Location: mainhtml.php?post_created=success");
             exit();
         } else {
